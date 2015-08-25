446964b8da72fc95248137a83403ec9b384ac9a4
Konstantin Kononenko <kastantin@gmail.com>
2015-08-25 20:41:46 +0300

    Убрал старое объявление класса

--- hw8_kon.rb
+++ hw8_kon.rb
@@ -1,25 +1,4 @@
 #ДЗ №8
-# class TestId
-#   def test_typed(regexp)
-#     regexp =~ /\A[a-z][a-z0-9_]+\Z/
-#   end
-
-#   def test_class(regexp)
-#     regexp =~ /\A[A-Z][A-Za-z0-9_]+\Z/
-#   end
-
-#   def test_obj_option(regexp)
-#     regexp =~ /\A[@]{1}[a-z]+[a-z0-9_]*\Z/
-#   end
-
-#   def test_class_option(regexp)
-#     regexp =~ /\A[@]{2}[a-z]+[a-z0-9_]*\Z/
-#   end
-
-#   def test_const(regexp)
-#     regexp =~ /\A[A-Z][A-Z0-9_]*\Z/
-#   end
-# end
 
 class TestId
   def test_typed(regexp)
